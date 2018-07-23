8f8091b0e06c898c6125f9c95d6b5df828cb1058
Paris Tuzun <paristuzun@gmail.com>
2018-07-23 02:02:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,8 +34,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
-  return array
+  return array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
